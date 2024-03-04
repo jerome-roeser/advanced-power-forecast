31e9e5fb05d67b6da01ec15af45672c7225800dc
Marius Hoernschemeyer <hoernsch@posteo.de>
2024-03-04 18:02:10 +0100

    Added make command to test the module data in the main module

--- power/interface/main.py
+++ power/interface/main.py
@@ -4,6 +4,13 @@ from power.ml_ops.data import get_pv_data, clean_pv_data
 
 ###
 
-df = get_pv_data()
-df = clean_pv_data(df)
-df.head()
+def dummy():
+    df = get_pv_data()
+    df = clean_pv_data(df)
+    print(df.head(2))
+    pass
+
+
+if __name__ == '__main__':
+    dummy()
+
