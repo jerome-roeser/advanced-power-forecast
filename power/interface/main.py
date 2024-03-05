14d823a7aa187ed049db266b289d14b6bbad6fca
jerome roeser <jerome.roeser@gmail.com>
2024-03-05 15:47:09 +0100

    edita main and ml-ops model with yesterday baseline model

--- power/interface/main.py
+++ power/interface/main.py
@@ -1,5 +1,6 @@
 
 from power.ml_ops.data import get_pv_data, clean_pv_data
+from power.ml_ops.model import model_yesterday
 
 
 ###
@@ -11,6 +12,31 @@ def dummy():
     pass
 
 
-if __name__ == '__main__':
-    dummy()
+def create_yesterday_baseline(date, power_source='PV'):
+    """
+    create a simple baseline model based on the data form d-1.
+    """
 
+    pv_data = get_pv_data()
+    pv_data_clean = clean_pv_data(pv_data)
+    
+    yesterday_baseline = model_yesterday(pv_data_clean, date)
+    return yesterday_baseline
+
+def create_mean_baseline(date, power_source='PV'):
+    """
+    create a simple baseline model based on the data form d-1.
+    """
+
+    pv_data = get_pv_data()
+    pv_data_clean = clean_pv_data(pv_data)
+
+    # <YOUR CODE>
+
+    pass
+
+
+
+if __name__ == '__main__':
+    create_yesterday_baseline(date='1982-07-06 05:52:00', power_source='PV')
+    create_mean_baseline(date='1982-07-06 05:52:00', power_source='PV')
