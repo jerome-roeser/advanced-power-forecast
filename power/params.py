4d1d7c0abbd12ba8f977d0c735739739ef871312
jerome roeser <jerome.roeser@gmail.com>
2024-03-07 22:41:20 +0100

    edit makefile with reset files commands

--- power/params.py
+++ power/params.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 ##################  VARIABLES  ##################
-
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
 GCP_REGION = os.environ.get("GCP_REGION")
