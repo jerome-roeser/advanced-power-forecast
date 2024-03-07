611d72f36d217b86c3570a8f44232cca6a9ade6c
jerome roeser <jerome.roeser@gmail.com>
2024-03-07 22:32:14 +0100

    edit ml-ops registry

--- power/ml_ops/registry.py
+++ power/ml_ops/registry.py
@@ -7,9 +7,7 @@ from colorama import Fore, Style
 from tensorflow import keras
 from google.cloud import storage
 
-from taxifare.params import *
-import mlflow
-from mlflow.tracking import MlflowClient
+from power.params import *
 
 def save_results(params: dict, metrics: dict) -> None:
     """
@@ -18,12 +16,6 @@ def save_results(params: dict, metrics: dict) -> None:
     "{LOCAL_REGISTRY_PATH}/metrics/{current_timestamp}.pickle"
     - (unit 03 only) if MODEL_TARGET='mlflow', also persist them on MLflow
     """
-    if MODEL_TARGET == "mlflow":
-        if params is not None:
-            mlflow.log_params(params)
-        if metrics is not None:
-            mlflow.log_metrics(metrics)
-        print("✅ Results saved on MLflow")
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
 
@@ -57,29 +49,14 @@ def save_model(model: keras.Model = None) -> None:
 
     print("✅ Model saved locally")
 
-    if MODEL_TARGET == "gcs":
-        # 🎁 We give you this piece of code as a gift. Please read it carefully! Add a breakpoint if needed!
 
-        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
-        client = storage.Client()
-        bucket = client.bucket(BUCKET_NAME)
-        blob = bucket.blob(f"models/{model_filename}")
-        blob.upload_from_filename(model_path)
+    model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(f"models/{model_filename}")
+    blob.upload_from_filename(model_path)
 
-        print("✅ Model saved to GCS")
-
-        return None
-
-    if MODEL_TARGET == "mlflow":
-        mlflow.tensorflow.log_model(
-            model=model,
-            artifact_path="model",
-            registered_model_name=MLFLOW_MODEL_NAME
-        )
-
-        print("✅ Model saved to MLflow")
-
-        return None
+    print("✅ Model saved to GCS")
 
     return None
 
@@ -116,7 +93,6 @@ def load_model(stage="Production") -> keras.Model:
         return latest_model
 
     elif MODEL_TARGET == "gcs":
-        # 🎁 We give you this piece of code as a gift. Please read it carefully! Add a breakpoint if needed!
         print(Fore.BLUE + f"\nLoad latest model from GCS..." + Style.RESET_ALL)
 
         client = storage.Client()
@@ -137,79 +113,5 @@ def load_model(stage="Production") -> keras.Model:
 
             return None
 
-    elif MODEL_TARGET == "mlflow":
-        print(Fore.BLUE + f"\nLoad [{stage}] model from MLflow..." + Style.RESET_ALL)
-
-        # Load model from MLflow
-        model = None
-        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-        client = MlflowClient()
-
-        try:
-            model_versions = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[stage])
-            model_uri = model_versions[0].source
-
-            assert model_uri is not None
-        except:
-            print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {stage}")
-
-            return None
-
-        model = mlflow.tensorflow.load_model(model_uri=model_uri)
-
-        print("✅ Model loaded from MLflow")
-        return model
     else:
         return None
-
-
-
-def mlflow_transition_model(current_stage: str, new_stage: str) -> None:
-    """
-    Transition the latest model from the `current_stage` to the
-    `new_stage` and archive the existing model in `new_stage`
-    """
-    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-
-    client = MlflowClient()
-
-    version = client.get_latest_versions(name=MLFLOW_MODEL_NAME, stages=[current_stage])
-
-    if not version:
-        print(f"\n❌ No model found with name {MLFLOW_MODEL_NAME} in stage {current_stage}")
-        return None
-
-    client.transition_model_version_stage(
-        name=MLFLOW_MODEL_NAME,
-        version=version[0].version,
-        stage=new_stage,
-        archive_existing_versions=True
-    )
-
-    print(f"✅ Model {MLFLOW_MODEL_NAME} (version {version[0].version}) transitioned from {current_stage} to {new_stage}")
-
-    return None
-
-
-def mlflow_run(func):
-    """
-    Generic function to log params and results to MLflow along with TensorFlow auto-logging
-
-    Args:
-        - func (function): Function you want to run within the MLflow run
-        - params (dict, optional): Params to add to the run in MLflow. Defaults to None.
-        - context (str, optional): Param describing the context of the run. Defaults to "Train".
-    """
-    def wrapper(*args, **kwargs):
-        mlflow.end_run()
-        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-        mlflow.set_experiment(experiment_name=MLFLOW_EXPERIMENT)
-
-        with mlflow.start_run():
-            mlflow.tensorflow.autolog()
-            results = func(*args, **kwargs)
-
-        print("✅ mlflow_run auto-log done")
-
-        return results
-    return wrapper
