f4aa03e9d69a68182a58a2b31bcee81e398e638e
Ali Kakavand <a7.kakavand@gmail.com>
2024-03-07 14:45:09 +0100

    add cross_val module and a notebook for cross validation for internal use

--- power/ml_ops/cross_val.py
+++ power/ml_ops/cross_val.py
@@ -1,16 +1,4 @@
 
-# fold_length
-# fold_stride
-# train_test_ratio
-# output_length
-# n_targets
-# sequence_stride (option1 for sequencing)
-
-# input_length
-# n_features
-
-
-
 # importing ###################################################################
 
 # Data manipulation
@@ -18,20 +6,11 @@ import numpy as np
 import pandas as pd
 pd.set_option("display.max_columns", None)
 
-# Data Retrieval
-from power.ml_ops.data import clean_pv_data, get_pv_data
-
-# System
-import os
-
 # Manipulating temporal data and check the types of variables
 from typing import Dict, List, Tuple, Sequence
 
-# Early stopping
-from tensorflow.keras.callbacks import EarlyStopping
 
-
-# Creating FOLDs for cross validation #########################################
+# Creating FOLDs ##############################################################
 
 def get_folds(
     df: pd.DataFrame,
@@ -90,6 +69,7 @@ def get_X_y_strides(fold: pd.DataFrame, input_length: int, output_length: int, s
     - returns a list of sequences, each as a 2D-array time series
     '''
 
+    TARGET = 'power'
     X, y = [], []
 
     for i in range(0, len(fold), sequence_stride):
@@ -114,7 +94,9 @@ def get_Xi_yi(
     '''
     - given a fold, it returns one sequence (X_i, y_i)
     - with the starting point of the sequence being chosen at random
+    - TARGET is the variable(s) we want to predict (name of the column(s))
     '''
+    TARGET = 'power'
     first_possible_start = 0                                                    # the +1 accounts for the index, that is exclusive.
     last_possible_start = len(fold) - (input_length + output_length) + 1        # It can start as long as there are still
                                                                                 # 48 + 1 days after the 1st day.
@@ -126,11 +108,15 @@ def get_Xi_yi(
 
     return (X_i, y_i)
 
-def get_X_y(
+def get_X_y_seq(
     fold:pd.DataFrame,
     number_of_sequences:int,
     input_length:int,
     output_length:int):
+    '''
+    Given a fold, it creates a series of sequences randomly
+    as many as being specified
+    '''
 
     X, y = [], []                                                 # lists for the sequences for X and y
 
@@ -140,101 +126,3 @@ def get_X_y(
         y.append(yi)
 
     return np.array(X), np.array(y)
-
-
-
-# Cross validation ############################################################
-
-def cross_validation(cross_val_dict):
-    '''
-    This function cross-validates
-    - the "last seen value" baseline model
-    - the RNN model
-
-    input parameters:
-    '''
-
-    # fold_length
-    # fold_stride
-    # train_test_ratio
-    # output_length
-    # n_targets
-    # sequence_stride (option1 for sequencing)
-
-    if cross_val_dict == {}:
-        cross_val_dict = {
-            'fold_length': 52560,           # 6 years
-            'fold_stride': 52560,           # 6 years
-            'train_test_ratio': 0.83,       # 5 yrs/6 yrs
-            'output_length': 24,            # Day-ahead predictions
-            'sequence_stride': 72,          # 3 days
-            'n_seq_train': 250,             # number_of_sequences_train
-            'n_seq_test': 50                # number_of_sequences_test
-        }
-
-    fold_length = cross_val_dict['fold_length']
-    fold_stride = cross_val_dict['fold_stride']
-    train_test_ratio = cross_val_dict['train_test_ratio']
-    output_length = cross_val_dict['output_length']
-    sequence_stride = cross_val_dict['sequence_stride']
-    n_seq_train = cross_val_dict['n_seq_train']
-    n_seq_test = cross_val_dict['n_seq_test']
-
-
-    list_of_mae_baseline_model = []
-    list_of_mae_recurrent_model = []
-
-    # 0 - Creating folds
-    # =========================================
-    folds = get_folds(pv_df, fold_length, fold_stride)  # function we coded to get the folds
-
-    for fold_id, fold in enumerate(folds):
-
-        # 1 - Train/Test split the current fold
-        # =========================================
-        (fold_train, fold_test) = train_test_split(fold, train_test_ratio, INPUT_LENGTH) # function we coded to split train/test
-
-        n_seq_train = 250   # number_of_sequences_train
-        n_seq_test =  50    # number_of_sequences_test
-
-        X_train, y_train = get_X_y(fold_train, n_seq_train, INPUT_LENGTH, output_length)
-        X_test, y_test = get_X_y(fold_test, n_seq_test, INPUT_LENGTH, output_length)
-
-        X_train = np.delete(X_train, [0, 1], 2)
-        X_test = np.delete(X_test, [0, 1], 2)
-
-        X_train = X_train.astype('float32')
-        X_test = X_test.astype('float32')
-
-        # 2 - Modelling
-        # =========================================
-
-        ##### Baseline Model
-        baseline_model = init_baseline()
-        mae_baseline = baseline_model.evaluate(X_test, y_test, verbose=0)[1]   # evaluating baseline model (metric)
-        list_of_mae_baseline_model.append(mae_baseline)
-        print("-"*50)
-        print(f"MAE baseline fold n°{fold_id} = {round(mae_baseline, 2)}")
-
-        ##### LSTM Model
-        model = init_model(X_train, y_train)
-        es = EarlyStopping(monitor = "val_mae",
-                           mode = "min",
-                           patience = 5,
-                           restore_best_weights = True)
-        history = model.fit(X_train, y_train,
-                            validation_split = 0.3,
-                            shuffle = False,
-                            batch_size = 32,
-                            epochs = 50,
-                            callbacks = [es],
-                            verbose = 0)
-        res = model.evaluate(X_test, y_test, verbose=0)    # evaluating LSTM (metric)
-        mae_lstm = res[1]
-        list_of_mae_recurrent_model.append(mae_lstm)
-        print(f"MAE LSTM fold n°{fold_id} = {round(mae_lstm, 2)}")
-
-        ##### Comparison LSTM vs Baseline for the current fold
-        print(f"improvement over baseline: {round((1 - (mae_lstm/mae_baseline))*100,2)} % \n")
-
-    return list_of_mae_baseline_model, list_of_mae_recurrent_model
