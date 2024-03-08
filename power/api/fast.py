1e7159ad3865fb9d219a1b479d2a3e870d67ba94
jerome roeser <jerome.roeser@gmail.com>
2024-03-08 16:42:25 +0100

    bug with api

--- power/api/fast.py
+++ power/api/fast.py
@@ -1,13 +1,32 @@
 import pandas as pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from power.ml_ops.data import get_pv_data, clean_pv_data
+from power.ml_ops.data import get_pv_data, clean_pv_data, get_data_with_cache
 from power.ml_ops.model import model_yesterday
+from power.ml_ops.registry import load_model
 from datetime import datetime, timedelta
+
+from pathlib import Path
+from power.params import *
 
 app = FastAPI()
 app.state.data_pv = get_pv_data()
 app.state.data_pv_clean = clean_pv_data(app.state.data_pv)
+# data_processed_cache_path = Path(LOCAL_DATA_PATH).joinpath("processed", f"processed_pv.csv")
+
+# query = f"""
+#     SELECT *
+#     FROM {GCP_PROJECT}.{BQ_DATASET}.processed_pv
+#     ORDER BY utc_time
+# """
+# app.state.data_pv_clean = get_data_with_cache(
+#     gcp_project=GCP_PROJECT,
+#     query=query,
+#     cache_path=data_processed_cache_path,
+#     data_has_header=True
+# )
+
+# app.state.model = load_model()
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -18,26 +37,26 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get("/predict")
-def predict(
-    given_date: str,  # 2013-07-06
-    ):
+# @app.get("/predict/huajie")
+# def predict(
+#     given_date: str,  # 2013-07-06
+#     ):
 
-    df= pd.read_csv('raw_data/1980-2022_pv.csv')
-    given_date_converted = datetime.strptime(given_date, "%Y-%m-%d")
-    one_day_before = given_date_converted - timedelta(days=1)
-    given_date_before = one_day_before.strftime("%Y-%m-%d")
-    given_date_dt = pd.to_datetime(given_date_before)
-    df['local_time'] = pd.to_datetime(df['local_time'], utc=True)
-    selected_data = df[df['local_time'].dt.date == pd.to_datetime(given_date_dt).date()]
-    new_df = pd.DataFrame({
-        'local_time': selected_data['local_time'].dt.strftime('%Y-%m-%d %H:%M:%S'),
-        'electricity': selected_data['electricity']
-    })
-    df_dict = new_df.to_dict(orient='records')
-    converted_dict = {entry['local_time']: entry['electricity'] for entry in df_dict}
+#     df= pd.read_csv('raw_data/1980-2022_pv.csv')
+#     given_date_converted = datetime.strptime(given_date, "%Y-%m-%d")
+#     one_day_before = given_date_converted - timedelta(days=1)
+#     given_date_before = one_day_before.strftime("%Y-%m-%d")
+#     given_date_dt = pd.to_datetime(given_date_before)
+#     df['local_time'] = pd.to_datetime(df['local_time'], utc=True)
+#     selected_data = df[df['local_time'].dt.date == pd.to_datetime(given_date_dt).date()]
+#     new_df = pd.DataFrame({
+#         'local_time': selected_data['local_time'].dt.strftime('%Y-%m-%d %H:%M:%S'),
+#         'electricity': selected_data['electricity']
+#     })
+#     df_dict = new_df.to_dict(orient='records')
+#     converted_dict = {entry['local_time']: entry['electricity'] for entry in df_dict}
 
-    return converted_dict
+#     return converted_dict
 
 @app.get("/predict/previous_value")
 def predict_previous_value(input_date: str):
@@ -45,6 +64,63 @@ def predict_previous_value(input_date: str):
     yesterday_baseline = model_yesterday(pv_data_clean, input_date)
     values = yesterday_baseline.get('electricity').to_list()
     return {input_date: values}
+
+# @app.get("/predict")
+# def predict(input_date: str, n_days=2):
+#     pv_data_clean = app.state.data_pv_clean
+#     input_timestamp = pd.Timestamp(input_date, tz='UTC')
+#     idx = pv_data_clean[pv_data_clean.utc_time == input_timestamp].index[0]
+
+#     n_rows = 24 * n_days
+#     if idx <= n_rows:
+#         df = pv_data_clean.iloc[0:idx,:]
+#     else:
+#         df = pv_data_clean.iloc[idx-n_rows:idx,:].reset_index()
+
+#     model = app.state.model
+#     assert model is not None
+
+#     y_pred = model.predict(df)
+
+#     predicted_data = {
+#         'utc_time':XXX.get('utc_time').tolist(),
+#         'local_time':XXX.get('local_time').tolist(),
+#         'electricity':XXX.get('electricity').tolist()
+#     }
+
+#     return {'predicted_data': predicted_data}
+
+
+# def predict(
+#         pickup_datetime: str,  # 2013-07-06 17:18:00
+#         pickup_longitude: float,    # -73.950655
+#         pickup_latitude: float,     # 40.783282
+#         dropoff_longitude: float,   # -73.984365
+#         dropoff_latitude: float,    # 40.769802
+#         passenger_count: int
+#     ):      # 1
+#     """
+#     Make a single course prediction.
+#     Assumes `pickup_datetime` is provided as a string by the user in "%Y-%m-%d %H:%M:%S" format
+#     Assumes `pickup_datetime` implicitly refers to the "US/Eastern" timezone (as any user in New York City would naturally write)
+#     """
+
+#     X_pred = pd.DataFrame(dict(
+#         pickup_datetime=[pd.Timestamp(pickup_datetime, tz='US/Eastern')],
+#         pickup_longitude=[pickup_longitude],
+#         pickup_latitude=[pickup_latitude],
+#         dropoff_longitude=[dropoff_longitude],
+#         dropoff_latitude=[dropoff_latitude],
+#         passenger_count=[passenger_count],
+#         ))
+
+#     model = app.state.model
+#     assert model is not None
+
+#     X_processed = preprocess_features(X_pred)
+#     y_pred = float(model.predict(X_processed))
+
+#     return  {'fare_amount': round(y_pred, 2)}
 
 
 @app.get("/extract_data")
@@ -58,6 +134,7 @@ def extract_pv_data(input_date: str, n_days=10):
         df = pv_data_clean.iloc[0:idx+24,:]
     else:
         df = pv_data_clean.iloc[idx-n_rows:idx+24,:].reset_index()
+
     extracted_data = {
         'utc_time':df.get('utc_time').tolist(),
         'local_time':df.get('local_time').tolist(),
--- ui/app.py
+++ ui/app.py
@@ -21,7 +21,7 @@ st.sidebar.markdown(f"""
 
 prediction_date = st.sidebar.date_input(
                             label='Power prediction date',
-                            value=datetime.date(2020, 1, 1),
+                            value=datetime.date(2010, 1, 1),
                             min_value=datetime.date(1980, 1, 10),
                             max_value=datetime.date(2020, 1, 1),
                             )
@@ -44,27 +44,65 @@ location = locations.radio("Locations", ["Berlin - Tempelhof", "Berlin - Tegel",
 
 # make api call
 base_url = "http://127.0.0.1:8000"
-endpoint_model = "/predict/previous_value"
-endpoint_data = "/extract_data"
-url_model = f"{base_url}{endpoint_model}"
-url_data = f"{base_url}{endpoint_data}"
 
-params ={
+# endpoints
+endpoint_data = "/extract_data"
+endpoint_baseline = "/predict/previous_value"
+endpoint_model = "/predict"
+
+# urls
+url_data = f"{base_url}{endpoint_data}"
+url_baseline = f"{base_url}{endpoint_baseline}"
+url_model = f"{base_url}{endpoint_model}"
+
+params = {
     'input_date':input_prediction_date
     }
 
-response_model = requests.get(url_model, params=params).json()
-model_df = pd.DataFrame(response_model)
-
+# responses
 response_data = requests.get(url_data, params=params).json()
 data_df = pd.DataFrame(response_data)
 
+response_baseline = requests.get(url_baseline, params=params).json()
+baseline_df = pd.DataFrame(response_baseline)
+
+# response_model = requests.get(url_model, params=params).json()
+# model_df = pd.DataFrame(response_model)
 
 
 # Main Panel
 # Write name of chosen location
 st.write(f"**Chosen location:** :red[{location}]")
 
+
+# Graph for PV data
+hours_to_display = 24 * days_to_display
+
+# set-up 3 DatFrames according to input date and type of model
+X = data_df.iloc[240-hours_to_display:-24,:]
+y = data_df.iloc[-24:,:]
+y_baseline = baseline_df.set_index(np.arange(240,240+24))
+# y_model = model_df.set_index(np.arange(240,240+24))
+
+fig, ax = plt.subplots()
+ax.plot(X.get('electricity'), label='current production data')
+ax.plot(y.get('electricity'), label='true data')
+ax.plot(y_baseline.get(input_prediction_date), label='baseline data_API')
+ax.legend()
+plt.ylim(0,1)
+plt.xlim(0,265)
+st.pyplot(fig)
+
+
+# Metrics
+mean_training = X.get('electricity').mean()
+mean_predicted = y_model.get(input_prediction_date).mean()
+mean_diff = mean_predicted - mean_training
+
+# Trick to use 4 columns to display the metrics centered below graph
+col1, col2, col3, col4 = st.columns(4)
+col2.metric("Training", round(mean_training,3), "")
+col3.metric("Predicted", round(mean_predicted,3), round(mean_diff,3))
 
 
 # Map with the location
@@ -82,33 +120,3 @@ folium.Marker([coordinates[location]['lat'], coordinates[location]['lon']],
               popup=location,
               icon=folium.Icon(color='red')).add_to(map)
 folium_static(map)
-
-
-# Graph for PV data
-hours_to_display = 24 * days_to_display
-
-# set-up 3 DatFrames according to input date and type of model
-X = data_df.iloc[240-hours_to_display:-24,:]
-y = data_df.iloc[-24:,:]
-y_model = model_df.set_index(np.arange(240,240+24))
-
-
-fig, ax = plt.subplots()
-ax.plot(X.get('electricity'), label='current production data')
-ax.plot(y.get('electricity'), label='true data')
-ax.plot(y_model.get(input_prediction_date), label='baseline data_API')
-ax.legend()
-plt.ylim(0,1)
-plt.xlim(0,265)
-st.pyplot(fig)
-
-
-# Metrics
-mean_training = X.get('electricity').mean()
-mean_predicted = y_model.get(input_prediction_date).mean()
-mean_diff = mean_predicted - mean_training
-
-# Trick to use 4 columns to display the metrics centered below graph
-col1, col2, col3, col4 = st.columns(4)
-col2.metric("Training", round(mean_training,3), "")
-col3.metric("Predicted", round(mean_predicted,3), round(mean_diff,3))
