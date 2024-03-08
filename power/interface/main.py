0ce682a122b0c637d7f639cc34e33049f49b2311
jerome roeser <jerome.roeser@gmail.com>
2024-03-08 10:31:13 +0100

    create new main.py

--- power/interface/main.py
+++ power/interface/main.py
@@ -1,157 +1,256 @@
+import numpy as np
+import pandas as pd
 
-from tensorflow.keras.callbacks import EarlyStopping
+from pathlib import Path
+from colorama import Fore, Style
+from dateutil.parser import parse
 
-from power.ml_ops.data import get_pv_data, clean_pv_data, select_years
-from power.ml_ops.model import model_yesterday, init_RNN, init_baseline_yesterday
-from power.ml_ops.cross_val import get_X_y
+from power.params import *
+from power.ml_ops.data import get_data_with_cache, clean_data, load_data_to_bq
+from power.ml_ops.model import initialize_model, compile_model, train_model, evaluate_model
+from power.ml_ops.registry import load_model, save_model, save_results
+from power.ml_ops.registry import mlflow_run, mlflow_transition_model
+
+def preprocess(min_date:str = '2009-01-01', max_date:str = '2015-01-01') -> None:
+    """
+    - Query the raw dataset from Le Wagon's BigQuery dataset
+    - Cache query result as a local CSV if it doesn't exist locally
+    - Process query data
+    - Store processed data on your personal BQ (truncate existing table if it exists)
+    - No need to cache processed data as CSV (it will be cached when queried back from BQ during training)
+    """
+
+    print(Fore.MAGENTA + "\n ⭐️ Use case: preprocess" + Style.RESET_ALL)
+
+    # Query raw data from BUCKET BigQuery using `get_data_with_cache`
+    min_date = parse(min_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+    max_date = parse(max_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+
+    query = f"""
+        SELECT {",".join(COLUMN_NAMES_RAW)}
+        FROM {GCP_PROJECT_WAGON}.{BQ_DATASET}.raw_{DATA_SIZE}
+        WHERE pickup_datetime BETWEEN '{min_date}' AND '{max_date}'
+        ORDER BY pickup_datetime
+    """
+
+    # Retrieve data using `get_data_with_cache`
+    data_query_cache_path = Path(LOCAL_DATA_PATH).joinpath("raw", f"query_{min_date}_{max_date}_{DATA_SIZE}.csv")
+    data_query = get_data_with_cache(
+        query=query,
+        gcp_project=GCP_PROJECT,
+        cache_path=data_query_cache_path,
+        data_has_header=True
+    )
+
+    # Process data
+    data_clean = clean_data(data_query)
+
+    X = data_clean.drop("fare_amount", axis=1)
+    y = data_clean[["fare_amount"]]
+
+    X_processed = preprocess_features(X)
+
+    # Load a DataFrame onto BigQuery containing [pickup_datetime, X_processed, y]
+    # using data.load_data_to_bq()
+    data_processed_with_timestamp = pd.DataFrame(np.concatenate((
+        data_clean[["pickup_datetime"]],
+        X_processed,
+        y,
+    ), axis=1))
+
+    load_data_to_bq(
+        data_processed_with_timestamp,
+        gcp_project=GCP_PROJECT,
+        bq_dataset=BQ_DATASET,
+        table=f'processed_{DATA_SIZE}',
+        truncate=True
+    )
+
+    print("✅ preprocess() done \n")
+
+@mlflow_run
+def train(
+        min_date:str = '2009-01-01',
+        max_date:str = '2015-01-01',
+        split_ratio: float = 0.02, # 0.02 represents ~ 1 month of validation data on a 2009-2015 train set
+        learning_rate=0.0005,
+        batch_size = 256,
+        patience = 2
+    ) -> float:
+
+    """
+    - Download processed data from your BQ table (or from cache if it exists)
+    - Train on the preprocessed dataset (which should be ordered by date)
+    - Store training results and model weights
+
+    Return val_mae as a float
+    """
+
+    print(Fore.MAGENTA + "\n⭐️ Use case: train" + Style.RESET_ALL)
+    print(Fore.BLUE + "\nLoading preprocessed validation data..." + Style.RESET_ALL)
+
+    min_date = parse(min_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+    max_date = parse(max_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+
+    # Load processed data using `get_data_with_cache` in chronological order
+    # Try it out manually on console.cloud.google.com first!
+
+    # Below, our columns are called ['_0', '_1'....'_66'] on BQ, student's column names may differ
+    query = f"""
+        SELECT * EXCEPT(_0)
+        FROM {GCP_PROJECT}.{BQ_DATASET}.processed_{DATA_SIZE}
+        WHERE _0 BETWEEN '{min_date}' AND '{max_date}'
+        ORDER BY _0 ASC
+    """
+
+    data_processed_cache_path = Path(LOCAL_DATA_PATH).joinpath("processed", f"processed_{min_date}_{max_date}_{DATA_SIZE}.csv")
+    data_processed = get_data_with_cache(
+        gcp_project=GCP_PROJECT,
+        query=query,
+        cache_path=data_processed_cache_path,
+        data_has_header=False
+    )
+
+    if data_processed.shape[0] < 10:
+        print("❌ Not enough processed data retrieved to train on")
+        return None
+
+    # Create (X_train_processed, y_train, X_val_processed, y_val)
+    train_length = int(len(data_processed)*(1-split_ratio))
+
+    data_processed_train = data_processed.iloc[:train_length, :].sample(frac=1).to_numpy()
+    data_processed_val = data_processed.iloc[train_length:, :].sample(frac=1).to_numpy()
+
+    X_train_processed = data_processed_train[:, :-1]
+    y_train = data_processed_train[:, -1]
+
+    X_val_processed = data_processed_val[:, :-1]
+    y_val = data_processed_val[:, -1]
+
+    # Train model using `model.py`
+    model = load_model()
+
+    if model is None:
+        model = initialize_model(input_shape=X_train_processed.shape[1:])
+
+    model = compile_model(model, learning_rate=learning_rate)
+    model, history = train_model(
+        model, X_train_processed, y_train,
+        batch_size=batch_size,
+        patience=patience,
+        validation_data=(X_val_processed, y_val)
+    )
+
+    val_mae = np.min(history.history['val_mae'])
+
+    params = dict(
+        context="train",
+        training_set_size=DATA_SIZE,
+        row_count=len(X_train_processed),
+    )
+
+    # Save results on the hard drive using power.ml_ops.registry
+    save_results(params=params, metrics=dict(mae=val_mae))
+
+    # Save model weight on the hard drive (and optionally on GCS too!)
+    save_model(model=model)
+
+    # The latest model should be moved to staging
+    if MODEL_TARGET == 'mlflow':
+        mlflow_transition_model(current_stage="None", new_stage="Staging")
+
+    print("✅ train() done \n")
+
+    return val_mae
 
 
-###
+@mlflow_run
+def evaluate(
+        min_date:str = '2014-01-01',
+        max_date:str = '2015-01-01',
+        stage: str = "Production"
+    ) -> float:
+    """
+    Evaluate the performance of the latest production model on processed data
+    Return MAE as a float
+    """
+    print(Fore.MAGENTA + "\n⭐️ Use case: evaluate" + Style.RESET_ALL)
 
-def preprocess_pv() -> None:
-  """
-  Check availability of traing and test data at cache or big query. If data is
-  not available from cache or big query, then preprocess raw data from local
-  folder and upload the data to big query and save it in the cache.
-  Input:
-    - No function input
-    - Try to load data from different sources in the following order:
-      1. Load traing and test data from cache folder
-      2. Load traing and test data from google big query
-      3. Load raw data from local folder
-    - Enviromental variables used:
-      - N_SEQUENCES_TRAIN = 250     # number_of_sequences_train
-      - OUTPUT_LENGTH = 24          # predict 24 hours
-      - TRAIN_TEST_RATIO = 0.83     # 5 years train, 1 year test
-      - INPUT_LENGTH = 24 * 2       # records every hour (24 hours) x 2 days = 48
-  Output:
-    - No function output
-    - Upload data to google big query and save to cache
-  """
-  # ----------------------------------------------------------------------------
-  ### Check availability of traing and test data from cache folder
+    model = load_model(stage=stage)
+    assert model is not None
 
-  # TODO: check for test_df, train_val_df, X_train, y_train, X_test, y_test
+    min_date = parse(min_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
+    max_date = parse(max_date).strftime('%Y-%m-%d') # e.g '2009-01-01'
 
-  # ----------------------------------------------------------------------------
-  ### Check availability of traing and test data from big query
+    # Query your BigQuery processed table and get data_processed using `get_data_with_cache`
+    query = f"""
+        SELECT * EXCEPT(_0)
+        FROM {GCP_PROJECT}.{BQ_DATASET}.processed_{DATA_SIZE}
+        WHERE _0 BETWEEN '{min_date}' AND '{max_date}'
+    """
 
-  # TODO: check for test_df, train_df, X_train, y_train, X_test, y_test;
-  #       save test_df, train_df, X_train, y_train, X_test, y_test to cache
+    data_processed_cache_path = Path(f"{LOCAL_DATA_PATH}/processed/processed_{min_date}_{max_date}_{DATA_SIZE}.csv")
+    data_processed = get_data_with_cache(
+        gcp_project=GCP_PROJECT,
+        query=query,
+        cache_path=data_processed_cache_path,
+        data_has_header=False
+    )
 
-  # ----------------------------------------------------------------------------
-  ### Load raw data and preprocess it
-  df = get_pv_data()
-  df = clean_pv_data(df)
+    if data_processed.shape[0] == 0:
+        print("❌ No data to evaluate on")
+        return None
 
-  # Test train split the clean data
-  test_df       = select_years(df, start=2020, end=2022)
-  train_df  = select_years(df, start=2080, end=2019)
+    data_processed = data_processed.to_numpy()
 
-  # Preprocess
-  # to train the model
-  X_train, y_train = get_X_y(train_df,
-                            N_SEQUENCES_TRAIN, INPUT_LENGTH, OUTPUT_LENGTH)
+    X_new = data_processed[:, :-1]
+    y_new = data_processed[:, -1]
 
-  # to test the model
-  X_test, y_test = get_X_y(test_df,
-                            N_SEQUENCES_TRAIN, INPUT_LENGTH, OUTPUT_LENGTH)
+    metrics_dict = evaluate_model(model=model, X=X_new, y=y_new)
+    mae = metrics_dict["mae"]
 
-  ## Save preprocessed data to cache
+    params = dict(
+        context="evaluate", # Package behavior
+        training_set_size=DATA_SIZE,
+        row_count=len(X_new)
+    )
 
-  # TODO: save test_df, train_df, X_train, y_train, X_test, y_test to cache
+    save_results(params=params, metrics=metrics_dict)
 
-  ## Upload preprocessed data to google query
+    print("✅ evaluate() done \n")
 
-  # TODO: upload test_df, train_df, X_train, y_train, X_test, y_test to cache
-
-  # load_data_to_bq(
-  #       data_processed_with_timestamp,
-  #       gcp_project=GCP_PROJECT,
-  #       bq_dataset=BQ_DATASET,
-  #       table=f'processed_{DATA_SIZE}',
-  #       truncate=True
-  #   )
+    return mae
 
 
-def training(
-  validation_split: float = 0.3,
-  epochs=50,
-  batch_size=32,
-  patience=5
-  ) -> float:
-  """
-  First get training data from google big query or cache. Then train the
-  RNN model and store training results and the model.
-  Input:
-    - No mandaory function input; optional parameters to adjust the training
-  Output:
-    -
-  """
-  # ----------------------------------------------------------------------------
-  ### Load training data
+def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
+    """
+    Make a prediction using the latest trained model
+    """
 
-  # TODO: Get X_train, y_train from the cache
+    print("\n⭐️ Use case: predict")
 
-  # ----------------------------------------------------------------------------
-  ### Initialize model
-  model_RNN = init_RNN(X_train, y_train) # from module model inpackage ml_ops
+    if X_pred is None:
+        X_pred = pd.DataFrame(dict(
+        pickup_datetime=[pd.Timestamp("2013-07-06 17:18:00", tz='UTC')],
+        pickup_longitude=[-73.950655],
+        pickup_latitude=[40.783282],
+        dropoff_longitude=[-73.984365],
+        dropoff_latitude=[40.769802],
+        passenger_count=[1],
+    ))
 
-  # ----------------------------------------------------------------------------
-  ### train model
+    model = load_model()
+    assert model is not None
 
-  es = EarlyStopping(monitor = "val_mae",
-                     patience = patience,
-                     restore_best_weights = True)
+    X_processed = preprocess_features(X_pred)
+    y_pred = model.predict(X_processed)
 
-  history = model_RNN.fit(X_train, y_train,
-                      validation_split = validation_split,
-                      shuffle = False,
-                      batch_size = batch_size,
-                      epochs = epochs,
-                      callbacks = [es],
-                      verbose = 0)
-
-  # ----------------------------------------------------------------------------
-  ### Save result and model
-
-
-  print('# model training done')
-
-  return val_mae
-
+    print("\n✅ prediction done: ", y_pred, y_pred.shape, "\n")
+    return y_pred
 
 
 if __name__ == '__main__':
-  # execute function in the right order
-
-
-
-
-
-
-#create_yesterday_baseline(date='1982-07-06 05:00:00', power_source='PV')
-#create_mean_baseline(date='1982-07-06 05:00:00', power_source='PV')
-
-# def create_yesterday_baseline(date, power_source='PV'):
-#     """
-#     create a simple baseline model based on the data form d-1.
-#     """
-
-#     pv_data = get_pv_data()
-#     pv_data_clean = clean_pv_data(pv_data)
-
-#     yesterday_baseline = model_yesterday(pv_data_clean, date)
-#     return yesterday_baseline
-
-# def create_mean_baseline(date, power_source='PV'):
-#     """
-#     create a simple baseline model based on the data form d-1.
-#     """
-
-#     pv_data = get_pv_data()
-#     pv_data_clean = clean_pv_data(pv_data)
-
-#     # <YOUR CODE>
-
-#     pass
+    preprocess(min_date='2009-01-01', max_date='2015-01-01')
+    train(min_date='2009-01-01', max_date='2015-01-01')
+    evaluate(min_date='2009-01-01', max_date='2015-01-01')
+    pred()
--- power/params.py
+++ power/params.py
@@ -4,12 +4,12 @@ import numpy as np
 ##################  VARIABLES  ##################
 MODEL_TARGET = os.environ.get("MODEL_TARGET")
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
-GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
+GCP_PROJECT_JEROMEN = os.environ.get("GCP_PROJECT_JEROME")
 GCP_REGION = os.environ.get("GCP_REGION")
 BQ_DATASET = os.environ.get("BQ_DATASET")
 BQ_REGION = os.environ.get("BQ_REGION")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
-# 
+#
 GAR_IMAGE = os.environ.get("GAR_IMAGE")
 GAR_MEMORY = os.environ.get("GAR_MEMORY")
 
